7fcbb1325b47fb117b5eba79d799946bd4cf753f
Paul K <pkfis@outlook.com>
2020-01-29 05:19:55 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200129051918) do
+ActiveRecord::Schema.define(version: 20200129051948) do
 
 end
