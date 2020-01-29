a51c8f7fd17074befbee4f4d65509ff8aaf1ad79
Paul K <pkfis@outlook.com>
2020-01-29 05:20:55 +0000

    Automatically backed up by Learn

--- db/migrate/20200129051948_create_users.rb
+++ db/migrate/20200129051948_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
-  def change
+
     def change
-    create_table :users do |t|
-      t.string :username
-      t.string :password_digest
+      create_table :users do |t|
+        t.string :username
+        t.string :password_digest
+      end
     end
-  end
-  end
+
 end
